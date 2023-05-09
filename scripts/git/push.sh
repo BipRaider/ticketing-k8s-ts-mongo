b31083a1a7e22f1e57c28fb4ae2fbeba9f8d35f6
BipRaider <thebipus@gmail.com>
2023-05-09 16:43:27 +0300

    update

--- scripts/git/push.sh
+++ scripts/git/push.sh
@@ -8,8 +8,13 @@ git status
 echo [ Add a message to a commit? "Updated: "]
 read commit
 
-echo [ Git commit as: "Updated: ${commit}"]
-git commit -m "Updated: ${commit}"
+echo [ Git commit as: "${commit}"]
+if [ "${commit}" ]; then
+  git commit -m "${commit}"
+else
+  git commit -m "update"
+fi
+
 
 echo ["Need to raise version? [y\n]"]
 read version
