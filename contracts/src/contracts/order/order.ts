49096eee4f5282767b93424606a37b4a4ebf61bc
BipRaider <thebipus@gmail.com>
2023-05-11 16:13:56 +0300

    Updates.

--- contracts/src/contracts/order/order.ts
+++ contracts/src/contracts/order/order.ts
@@ -1,13 +1,13 @@
 export enum OrdersStatus {
-  /*** When the orted  has been created.
-   **  But the ticket it is trying to orden has not been reserved.
+  /*** When the order  has been created.
+   **  But the ticket it is trying to order has not been reserved.
    */
   Created = 'created',
   /*** The ticket the order is trying to reserve has already been reserved
-   **  Or when the user canselled the order
+   **  Or when the user cancelled the order
    **  The order expires before payment
    */
-  Cansellet = 'cansellet',
+  Cancelled = 'cancelled',
   /*** The order has reserved the ticket.
    ** And the user has provided payment successfully
    */
