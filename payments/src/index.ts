2100c5e1bf235c97b591e5fc7d9cc390e7ccf401
BipRaider <thebipus@gmail.com>
2023-05-25 21:56:39 +0300

    Added console log

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,8 @@ import { MongoService } from './database';
 import { natsWrapper } from './events';
 
 const start = async () => {
+  console.dir('Service running...');
+
   const SRV = process.env['SRV_NAME'];
   if (!SRV) throw new ErrorEx('Forbidden', 'SRV_NAME invalid', 403);
 
@@ -18,7 +20,7 @@ const start = async () => {
   if (!salt) throw new ErrorEx('JWT_SALT must be defined', null, 403);
   if (!dburl) throw new ErrorEx('MONGO_URL must be defined', null, 403);
 
-  console.log('Service start...');
+  console.dir('Service start...');
 
   await new MongoService().connect();
   await natsWrapper.init(natsClusterID, natsClientID, { url: natsUrl });
