80930ab9c80fc09641b7ae5870eba471d9fdaef6
BipRaider <thebipus@gmail.com>
2023-05-25 19:04:12 +0300

    Updated test the git action

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,14 +2,13 @@ import { ErrorEx } from '@bipdev/common';
 import { natsWrapper } from './events';
 
 const start = async () => {
+  console.dir('Service running...');
   const redisHost = process.env['REDIS_HOST'];
-
   if (!redisHost) throw new ErrorEx('REDIS_HOST must be defined', null, 403);
 
   const natsClusterID = process.env['NATS_CLUSTER_ID'];
   const natsClientID = process.env['NATS_CLIENT_ID'];
   const natsUrl = process.env['NATS_URL'];
-
   console.log('Service start...');
 
   await natsWrapper.init(natsClusterID, natsClientID, { url: natsUrl });
